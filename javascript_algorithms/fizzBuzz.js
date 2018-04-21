11a62a58f7594e9d7d88fbf9f1ad7c6deff21f2e
DARREN DHANPAT <darren.dhanpat@yahoo.com>
2018-04-21 00:08:47 -0400

    updated fizzBuzz

--- javascript_algorithms/fizzBuzz.js
+++ javascript_algorithms/fizzBuzz.js
@@ -11,14 +11,18 @@
 for (var i = 1; i <= 100; i++) {
 
 	 if((i % 3 == 0) && (i % 5 == 0)){
-	console.log(i + "FizzBuzz");
+	console.log(i + " " + "FizzBuzz");
 	}
 	else if( i % 3 == 0){
-		console.log( i + "Fizz");
+		console.log( i + " " + "Fizz");
 	}
 	else if (i % 5 == 0){
-		console.log(i + "Buzz")
+		console.log(i + " " + "Buzz")
 	}
 	else
 		console.log(i);
 }
+
+/* if((i % 3 == 0) && (i % 5 == 0)){
+	console.log(i + " " + "FizzBuzz");
+	}*/
